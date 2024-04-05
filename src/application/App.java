b878814f1a5e5b954db13864cd81fb5d436b4d44
Vinicius Fillos <vinifillos@gmail.com>
2024-04-05 10:49:23 -0300

    Interface implementation

---
+++ src/application/App.java
@@ -0,0 +1,26 @@
+package application;
+
+import entities.Product;
+import util.PriceUpadate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Locale.setDefault(Locale.US);
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.00));
+        list.add(new Product("Mouse", 50.00));
+        list.add(new Product("Tablet", 350.50));
+        list.add(new Product("HD Case", 80.90));
+
+        list.forEach(new PriceUpadate());
+
+        list.forEach(System.out::println);
+    }
+}

a20622f7c0687d10d04b6a104ea1c83ed73edcae
Vinicius Fillos <vinifillos@gmail.com>
2024-04-05 10:59:10 -0300

    Inline lambda expression

--- src/application/App.java
+++ src/application/App.java
@@ -22,11 +22,8 @@ public class App {
 
 
         double factor = 1.1;
-        Consumer<Product> cons = p -> {
-          p.setPrice(p.getPrice()*factor);
-        };
-
-        list.forEach(cons);
+        
+        list.forEach(p -> p.setPrice(p.getPrice()*factor));
 
         list.forEach(System.out::println);
     }
